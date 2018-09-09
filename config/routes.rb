810fbd066826e101088685aedfbb0eee53a51c9b
kwontaekhwan <taekani@gmail.com>
2018-09-09 05:57:04 +0000

    vaildation!

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   resources :contacts
   get '/' => 'home#index'
   get '/about' => 'home#about'
-  #get 'wellcome/index'
-  #resources :articles	
+  get 'wellcome/index'
+  resources :articles	
   #root 'wellcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
