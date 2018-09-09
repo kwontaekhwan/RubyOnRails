9a1bc7bcfee38f39453bc1ab26c08bd18d4cc564
kwontaekhwan <taekani@gmail.com>
2018-09-09 01:38:12 +0000

    Depot Scaffold first init

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180904132830) do
+ActiveRecord::Schema.define(version: 20180908124426) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 20180904132830) do
     t.string   "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "image_url"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
 end
